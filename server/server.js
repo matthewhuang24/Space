ae3ab2c97a38a2a8f7eba07036c8720b24ca8d2c
Christie Herring <christieherring@Christies-MacBook-Pro.local>
2021-10-16 15:54:28 -0700

    built server, handshake with database

--- server/server.js
+++ server/server.js
@@ -1,18 +1,26 @@
 const express = require('express');
-// const path = require('path');
-// const cors = require('cors');
+const path = require('path');
+const cors = require('cors');
+const db = require('./projectsContainer.js');
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json());
+app.use(express.urlencoded());
 
-// app.get('/', (req, res) => {
-//   return res.sendFile(path.resolve(__dirname, '../index.html'));
-// });
-
-app.use((req, res) => {
-  res.status(404);
+app.get('/', (req, res) => {
+  return res.sendFile(path.resolve(__dirname, '../index.html'));
 });
 
-app.listen(5000, () => console.log(`Server running on port 5000`));
+app.get('/projects', async (req, res) => {
+  console.log('in projects get request');
+  const searchResults = await db.query(
+    `SELECT * FROM projects WHERE technology='${req.body.technology}';`
+  ); //not sure if front end will give us data in this format
+  return res.status(200).json(searchResults.rows);
+});
+
+app.use((req, res) => res.status(404));
+
+app.listen(5001, () => console.log(`Server running on port 5001`));
