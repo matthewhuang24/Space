fc52729b6069c0e7fec2b3dd2c21415549b5778d
alecksy <alecks.lu@gmail.com>
2021-10-18 16:25:52 -0700

    finished front and back

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -2,8 +2,20 @@ import React, {useState, useEffect} from 'react';
 
 // check the props object, console.log
 const Card = (props) => {
+	console.log('props.info: ',props.info)
+	const name = props.info.projectname;
+	const tech = props.info.technology;
+	const description = props.info.description;
+
   return (
-		<div>{props.info}</div>
+		<div className='individualCard'>
+			<h3><b>Name: </b></h3>
+			<h4>{name}</h4> <br />
+			<h3><b>Technology: </b></h3>
+			<h4>{tech}</h4> <br />
+			<h3> <b>Description: </b></h3>
+			<h4 >{description}</h4>
+		</div>
 	)
 }
 
