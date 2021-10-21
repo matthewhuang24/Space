b65615001b194617f129e264fcb3ae3c7cc058dd
Ron Fu <ronfu85@gmail.com>
2021-10-20 19:29:57 -0700

    navbar w/logo, 2 btns, styling
    Co-authored: William Cheng

---
+++ client/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import NewWorkspace from '../components/buttons/NewWorkspace.jsx';
+import UploadWorkspace from '../components/buttons/UploadWorkspace.jsx';
+
+const Navbar = () => {
+
+return (
+  <nav className="flex justify-between bg-gray-200 bg-opacity-50 h-20 ">
+  <div className="flex items-center flex-shrink-0 text-black ml-8">
+    <svg className="fill-current h-8 w-8 mr-1" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/>  </svg>
+    <span className="font-semibold text-xl tracking-tight"><a href="http://localhost:8080">POGO</a></span>
+  </div>
+  <div className="w-full block .right-0 lg:flex lg:items-center lg:w-auto">
+    <div className="w-96 mr-8 flex justify-around">
+      <UploadWorkspace/>
+      <NewWorkspace/>
+    </div>
+  </div>
+</nav>
+)
+}
+
+export default Navbar;
