dbdf4db23e841284e66a5cf2737777e1d871279f
Sean Lee <lee.sw.sean@gmail.com>
2021-10-20 19:13:27 -0700

    Co-authored-by: William Cheng <WilliamCheng12345@users.noreply.github.com> Co-authored-by: Ron <ronfu85@gmail.com>

--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
+import { v4 as uuidv4 } from 'uuid';
+
 import UploadWorkspace from './buttons/UploadWorkspace';
 import NewWorkspace from './buttons/NewWorkspace';
 
 const Welcome = () => {
+  const key = uuidv4();
+
   return (
     <div className="md:container md:mx-auto flex flex-col justify-center items-center h-screen">
       <div className="text-5xl p-4">
         "Logo Goes Here"
       </div>
       <div className="w-96 flex justify-around p-2">
-        <UploadWorkspace />
-        <NewWorkspace />
+        <UploadWorkspace storageKey={key} />
+        <NewWorkspace storageKey={key} />
       </div>
     </div>
   )
